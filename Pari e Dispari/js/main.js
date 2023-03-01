1032e3c0bd77a448184ee8bd32b37956e3956fa0
NicholasConti <conti94@live.it>
2023-03-01 15:47:05 +0100

    input check 2

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 let check = false;
 let userChoice;
+//input check
 while (check === false){
     userChoice = prompt('Scegli tra "Pari" e "Dispari" , RICORDA  LE MAIUSCOLE!!');
     if ((userChoice === 'Pari') || (userChoice === 'Dispari')){
@@ -8,4 +9,16 @@ while (check === false){
     }  
 }
 
-console.log(userChoice);
+let numUser;
+let min = 1;
+let max = 5;
+check = false;
+//input check
+while (check === false){
+    numUser = Number(prompt('Scegli un numero tra 1 e 5'));
+    if ((numUser >= min) && (numUser <= max) && (!isNaN(numUser))){
+        check = true;
+    }  
+}
+
+console.log(numUser);
