ef2c50c04217b13fe0c9fc3492190de29f3f38a8
NicholasConti <conti94@live.it>
2023-03-01 17:01:53 +0100

    palindormo

--- Palindormo/js/main.js
+++ Palindormo/js/main.js
@@ -1,1 +1,31 @@
 "use strict";
+//input
+const word = prompt(' Inserisci una parola');
+
+let reverseWords = [];
+let x = 0 ;
+//scrivo la parola al contrario nell array
+for (let i = word.length - 1; i >= 0; i--){
+    reverseWords[x] = word[i];
+    x++;
+}
+//funzione per verificare se le parole nello stesso indice sono uguali
+function paliCheck(inputWord, wordReverse){
+    let check = false
+    for(let i = 0; i < inputWord.length; i++){
+        if(inputWord[i] === wordReverse[i]){
+            check = true;
+        } else{
+            check = false
+        }
+    }
+    return check;
+}
+
+const result = paliCheck(word, reverseWords);
+
+if (result === true){
+    alert('LA PAROLA E PALINDROMA');
+} else{
+    alert('LA PAROLA NON E PALINDROMA');
+}
